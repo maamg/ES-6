ccacc6a71922e06e0118038dc9bff3e1b3f8df53
maamg <maamg164@gmail.com>
2021-02-11 16:51:08 +0600

    3rd ES-6

---
+++ template.js
@@ -0,0 +1,11 @@
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+const fullName = firstName + ' ' + lastName ;
+const fullName2 = ` ${firstName} ${420} is a good boy`;
+console.log(fullName2);
+
+const multiline = `I love you 
+I miss you \n
+I habijabi you `;
+
+console.log(multiline);
