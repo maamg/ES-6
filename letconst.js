442e924a6eb7e94cdc658f2da1b42a62f34c4d48
maamg <maamg164@gmail.com>
2021-02-11 16:31:17 +0600

    firstES-6

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const hubby = 'Elias Kanchon';
+console.log(hubby);
+
+const numbers = [12, 15];
+numbers[0] = 1122;
+numbers.push(44);
+numbers.pop();
+//numbers= [12,15]; // error 
+//numbers = ['ami', 'tumi', 'se']
+const nayok = {name:'sakib khan', phone: 2353};
+console.log(numbers);
+let sum = 0;
+for( var i = 0; i<10; i++){
+    sum = sum + i;
+}
+console.log(i);
